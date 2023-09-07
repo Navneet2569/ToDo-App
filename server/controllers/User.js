a2f0ec76fc5a1a0b46883f84a73f8ca94c4ad232
Navneet2569 <nav9027388401singh@gmail.com>
2023-09-06 23:28:52 -0700

    Working on the backend

---
+++ server/controllers/User.js
@@ -0,0 +1,30 @@
+import { User } from "../models/users.js";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const { avatar } = req.files;
+
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const otp = Math.floor(1000000 * Math.random());
+
+    user = await User.create({
+      name,
+      email,
+      password,
+      avatar,
+      otp,
+      otp_expiry: new Date(Date.now() + process.env.OTP_EXPIRE * 60 * 1000),
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
